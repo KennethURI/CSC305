dbe237b4d69d098f4b8b22083605c1813e7f590b
Kenneth Lu Diaz <ludiazken@gmail.com>
2021-06-08 09:38:35 -0400

    Add rational2

--- src/main/java/week1/Rational.java
+++ src/main/java/week1/Rational.java
@@ -1,20 +1,48 @@
 package week1;
 
+/**
+ * This class represents a rational number object
+ * Useful as an argument to a function
+ */
 public class Rational {
-    int numerator, denominator;
+    private int numerator, denominator;
 
+    /**
+     * This class creates a Rational object
+     * @param numerator any number
+     * @param denominator should not be 0
+     */
     public Rational(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
     @Override
+    /**
+
+     * Checks whether the two Rationals have exactly the same values.
+
+     * Rational(1,2) and Rational(2,4) are NOT exactly the same!
+
+     *
+
+     * @param other some other Rational
+     * @return true when numerators and denominators are the same
+     */
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Rational rational = (Rational) o;
         return numerator == rational.numerator &&
                 denominator == rational.denominator;
+    }
+
+    /**
+     * Gets the double value of a rational class
+     * @return double value of rational
+     */
+    public double doubleValue() {
+        return (double) this.numerator/this.denominator;
     }
 
     public static void main(String[] args) {
@@ -24,6 +52,10 @@ public class Rational {
                 (rational12.equals(new Rational(1, 2)) ? "passed" : "failed"));
         System.out.println("Test rational12 not equals rational23: " +
                 (! rational12.equals(rational23) ? "passed" : "failed"));
+        System.out.println("Test rational12 < rational23 as doubles: " +
+
+                (rational12.doubleValue() < rational23.doubleValue() ?
+                        "passed" : "failed"));
     }
 
 }
